fc6631d8306e6477f8a62cfe323e252fd3c2feb7
Martin Drozdík <drozdik.m@seznam.cz>
2019-10-01 11:32:47 +0200

    Add sln, Add finished SimpleEvent, Add unfinished ParameterizableEvent

---
+++ Pozorovatel/ParameterizableEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pozorovatel
+{
+    class ParameterizableEvent<Subject, Args>
+    {
+        /*private List<> observerFunctions = new List<VoidFunction>();
+
+        /// <summary>
+        /// Metoda, která přidá nového pozorovatele (resp. jeho funkci)
+        /// </summary>
+        /// <param name="newObserverFunction"></param>
+        public void Add(VoidFunction newObserverFunction)
+        {
+            observerFunctions.Add(newObserverFunction);
+        }
+
+        /// <summary>
+        /// Metoda, která je volána subjektem a která notifikuje všechny pozorovatele
+        /// </summary>
+        public void Invoke()
+        {
+            foreach (var observerFunction in observerFunctions)
+                observerFunction();
+        }*/
+    }
+}
---
+++ Pozorovatel/SimpleEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pozorovatel
+{
+    class SimpleEvent
+    {
+        private List<VoidFunction> observerFunctions = new List<VoidFunction>();
+
+        /// <summary>
+        /// Metoda, která přidá nového pozorovatele (resp. jeho funkci)
+        /// </summary>
+        /// <param name="newObserverFunction"></param>
+        public void Add(VoidFunction newObserverFunction)
+        {
+            observerFunctions.Add(newObserverFunction);
+        }
+
+        /// <summary>
+        /// Metoda, která je volána subjektem a která notifikuje všechny pozorovatele
+        /// </summary>
+        public void Invoke()
+        {
+            foreach (var observerFunction in observerFunctions)
+                observerFunction();
+        }
+    }
+
+    delegate void VoidFunction();
+}
